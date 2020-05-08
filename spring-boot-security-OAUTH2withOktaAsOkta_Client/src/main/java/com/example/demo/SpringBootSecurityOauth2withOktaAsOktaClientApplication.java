6311dd91ea5e705984decce760de462a7842d02f
Mohit Darmwal <mohit.darmwal@sita.aero>
2020-05-08 11:37:29 +0530

    seventh

---
+++ spring-boot-security-OAUTH2withOktaAsOkta_Client/src/main/java/com/example/demo/SpringBootSecurityOauth2withOktaAsOktaClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityOauth2withOktaAsOktaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityOauth2withOktaAsOktaClientApplication.class, args);
+	}
+
+}
